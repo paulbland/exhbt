728f2385dd7c59d96ed159da00b2b4e4a0a28eae
Paul Bland <pbland@vsapartners.com>
2017-06-17 16:20:55 -0400

    getting user photos from ig api

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const request = require('request');
 const app = express()
 
+var accessToken;
+
 
 
 app.set('view engine', 'ejs');
@@ -13,8 +15,25 @@ app.get('/', function (req, res) {
 
 app.get('/ig', function (req, res) {
 
-	// If code recvd
-	if (req.query.code) {
+
+	if ( ! req.query.code) {
+
+		//
+		// NO CODE
+		//
+
+		res.render('ig', {
+			clientID 	: process.env.INSTAGRAM_CLIENT_ID,
+			redirectURL : process.env.INSTAGRAM_CALLBACK_URL,
+			code 		: req.query.code
+		});
+	}
+
+	else {
+
+		//
+		// WE HAVE ACCESS CODE
+		//
 
 		request.post({
 			url 	: 'https://api.instagram.com/oauth/access_token',
@@ -26,18 +45,24 @@ app.get('/ig', function (req, res) {
 				code : req.query.code
 			}
 		}, function (error, response, body) {
-			console.log('error:', error); // Print the error if one occurred
-			console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-			console.log('body:', body); // Print the HTML for the Google homepage.
+			//console.log('error:', error); // Print the error if one occurred
+			//console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+			//console.log('body:', body); // Print the HTML for the Google homepage.
+
+			var obj = JSON.parse(body);
+			accessToken = obj.access_token;
+
+			// get this users recent photos
+			request.get('https://api.instagram.com/v1/users/self/media/recent/?access_token='+accessToken, function (error, response, body) {
+				res.render('ig-result', {
+					photos	: JSON.parse(body);
+				});
+			});
+
 		});
 	}
 
-	// Show page
-	res.render('ig', {
-		clientID 	: process.env.INSTAGRAM_CLIENT_ID,
-		redirectURL : process.env.INSTAGRAM_CALLBACK_URL,
-		code 		: req.query.code
-	});
+
 })
 
 
